428445beb1b06368787b77636abbf4bee8528995
bbravo <bmauram2008@gmail.com>
2017-02-20 12:52:38 -0600

    Update Main.java

--- src/main/java/edu/jsu/mcis/Main.java
+++ src/main/java/edu/jsu/mcis/Main.java
@@ -7,17 +7,29 @@ public class Main {
     public static void main(String[] args) {
         Database dataBase = new Database();
         if (args.length == 2){
-			if(args[0].equals("Course"))
+			if(args[0].equals("course"))
 			{
 				System.out.println(dataBase.getCourse(Integer.parseInt(args[1])));
 			} 
-			else if(args[0].equals("Student"))
+			else if(args[0].equals("student"))
 			{
 				System.out.println(dataBase.getStudent(Integer.parseInt(args[1])).toString());
 			}
 			else{
 				System.out.println("");
 			}
+		}
+		else if (args.length == 1){
+			
+				if (args[0].equals("courseids"))
+				{
+					System.out.println(dataBase.getCourseIds());
+				}
+				else if (args[0].equals("studentids"))
+				{
+					System.out.println(dataBase.getStudentIds());
+				}
+			
 		}
 		else{
 			System.out.println("");
