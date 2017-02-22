d20d37bbd494ad7b0de77e1617f1cbe3b5f76c82
bbravo <bmauram2008@gmail.com>
2017-02-22 13:00:17 -0600

    Update Database.java

--- src/main/java/edu/jsu/mcis/Database.java
+++ src/main/java/edu/jsu/mcis/Database.java
@@ -6,133 +6,118 @@ import java.util.*;
   
 public class Database{
     
-	Map<Integer, Course> courseMap;
-	Map<Integer, Student> studentMap; 
-	ArrayList <String> courseIds = new ArrayList<String>();
-	ArrayList <String> studentIds = new ArrayList<String>();
+	private Map<Integer, Course> courseMap;
+	private Map<Integer, Student> studentMap; 
+	private ArrayList <String> courseIds = new ArrayList<String>();
+	private ArrayList <String> studentIds = new ArrayList<String>();
     
-    public Database()
-    {
+    public Database(){
         buildCourse("src\\main\\resources\\courses.csv");
+		addCourseInfo("src\\main\\resources\\courses");
         buildStudent("src\\main\\resources\\students.csv");
     }
     
-    public void buildCourse(String fileName)
-    {
-		/*
-		courseMap = new HashMap<Integer, Course>();
-  		String myline;
-    	try{
-        	BufferedReader in = new BufferedReader(new FileReader(new File(fileName)));
-            in.readLine();
-            while ((myline = in.readLine())!= null)
-            {
-             courseMap.put(Integer.parseInt(myline.split(",")[0].substring(1,myline.split(",")[0].length()-1)),new Course(Integer.parseInt(myline.split(",")[0].substring(1,myline.split(",")[0].length()-1)),
-                        					 Integer.parseInt(myline.split(",")[2].substring(1,myline.split(",")[2].length()-1)),
-                        					 Integer.parseInt(myline.split(",")[3].substring(1,myline.split(",")[3].length()-1)),
-                        					 myline.split(",")[1].substring(1,myline.split(",")[1].length()-1))); 	
-            }
-        }
-        catch(IOException e)
-        {e.printStackTrace();}
-		*/
-		
+    public void buildCourse(String fileName){
 		courseMap = new HashMap<Integer, Course>();
   		String myline;
     	try{
         	BufferedReader in = new BufferedReader(new FileReader(new File(fileName)));
             myline = in.readLine();
-            while ((myline = in.readLine())!=null)
-            {
-		
+            while ((myline = in.readLine())!=null){
 				String[] course = myline.split(",");
 				int id = Integer.parseInt(course[0].substring(1,course[0].length()-1));
 				int year = Integer.parseInt(course[2].substring(1,course[2].length()-1));
 				int size = Integer.parseInt(course[3].substring(1,course[3].length()-1));
 				String term = course[1].substring(1, course[1].length()-1);
 				courseMap.put(id, new Course(id, year, size, term));
-				
-				String ids = String.valueOf(id);
-				courseIds.add(ids);
-				
+				courseIds.add(String.valueOf(id));				
             }
         }
-        catch(IOException e) {e.printStackTrace();}
-		
+        catch(IOException e) {e.printStackTrace();}		
     }
-
-    public void buildStudent(String fileName){
-		/*
-		studentMap = new HashMap<Integer, Student>();
-    	String myline;
-        try{
-            BufferedReader in = new BufferedReader(new FileReader(new File(fileName)));
-			myline = in.readLine();
-			while ((myline = in.readLine())!= null){
-                if (myline.split(",")[0].length()<8){
-               		studentMap.put(Integer.parseInt(myline.split(",")[0].substring(1,myline.split(",")[0].length()-1)),new Student(Integer.parseInt(myline.split(",")[0].substring(1,myline.split(",")[0].length()-1)),
-                      								myline.split(",")[1],myline.split(",")[2],myline.split(",")[3]));
+	
+	public void addCourseInfo(String folderName){
+		final File folder = new File(folderName);
+		for (final File fileEntry : folder.listFiles()){
+			int courseID = Integer.parseInt(fileEntry.getName().substring(0,fileEntry.getName().length()-4));
+			try{
+				Course c = getCourse(courseID);
+				BufferedReader in = new BufferedReader(new FileReader(fileEntry));
+				String[] assignments = in.readLine().split(",");
+				ArrayList<String> lines = new ArrayList<String>();
+				String myline;
+				while ((myline = in.readLine())!=null){
+					lines.add(myline);
 				}
+				for (int i = 1; i < assignments.length; i++){
+					String assignmentName = assignments[i].substring(1, assignments[i].length()-1);
+					Assignment a = new Assignment(assignmentName);
+					for (int j = 0; j < lines.size(); j++){
+						String[] row = lines.get(j).split(",");
+						int studentId = Integer.parseInt(row[0].substring(1,row[0].length()-1));
+						int score = Integer.parseInt(row[i].substring(1,row[i].length()-1));
+						a.addStudentAndScore(studentId, score);
+					}
+					c.addAssignment(a);
+				}
+				courseMap.remove(courseID);
+				courseMap.put(courseID, c);
 			}
-        }
-        catch(IOException e) {e.printStackTrace();}
-		*/
-		
+			catch(IOException e) {e.printStackTrace();}
+		}
+	}
+	
+    public void buildStudent(String fileName){
 		studentMap = new HashMap<Integer, Student>();
     	String myline;
         try{
             BufferedReader in = new BufferedReader(new FileReader(new File(fileName)));
 			in.readLine();
-            while ((myline = in.readLine())!=null)
-            {
+            while ((myline = in.readLine())!=null){
 				String[] student = myline.split(",");
 				int id = Integer.parseInt(student[0].substring(1,student[0].length()-1));
 				String first = student[1].substring(1, student[1].length() - 1);
 				String last = student[2].substring(1, student[2].length() - 1);
 				String email = student[3].substring(1, student[3].length() - 1);
-				studentMap.put(id, new Student(id, first, last, email));
-				
-				String ids = String.valueOf(id);
-				studentIds.add(ids);
+				studentMap.put(id, new Student(id, first, last, email));				
+				studentIds.add(String.valueOf(id));
             }
         }
         catch(IOException e) {e.printStackTrace();}
     }
   
-
-    public Course getCourse(int id)
-    {
+    public Course getCourse(int id){
 		if(courseMap.containsKey(id)){
 			return courseMap.get(id);
 		}
 		throw new CourseException();
     }
 	
-    public Student getStudent(int id)
-    {
+    public Student getStudent(int id){
 		if(studentMap.containsKey(id)){
 			return studentMap.get(id);
 		}
 		throw new StudentException();
-               
     }
-    public ArrayList<String> getCourseList()
-    {
-        return courseIds;
-    }
+	
+	public ArrayList<String> getCourseList(){
+		return courseIds;		
+	}
+	
 	public String getCourseIds(){
 		String courses="";
-		for (int k=0; k<courseIds.size(); k++){
-			String course=(courseIds.get(k));
-			courses = courses+course +"\n";	
+		for (int i=0; i<courseIds.size(); i++){
+			String course=(courseIds.get(i));
+			courses += course +"\n";	
 		}
 		return courses;
 	}
+	
 	public String getStudentIds(){
 		String students = "";
-		for (int h=0; h<studentIds.size(); h++){
-			String student=(studentIds.get(h));
-			students = students+student + "\n";	
+		for (int i=0; i<studentIds.size(); i++){
+			String student=(studentIds.get(i));
+			students += student + "\n";	
 		}
 		return students;
 	}
