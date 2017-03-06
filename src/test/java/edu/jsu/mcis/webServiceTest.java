6eb5e69a6b89b5ea7a999f28b076506637780dbf
scortez85 <steven_cortez@msn.com>
2017-03-06 14:11:08 -0600

    added test for webservice

---
+++ src/test/java/edu/jsu/mcis/webServiceTest.java
@@ -0,0 +1,44 @@
+package edu.jsu.mcis;
+
+import org.junit.*;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.awt.*;
+import static org.junit.Assert.*;
+
+
+
+public class webServiceTest{
+
+	private webService testService = new webService();
+
+	@Test
+	public void testMethodsReturnObjects() throws MalformedURLException, IOException{
+
+		String studentID = testService.getStudent(111111);
+		String courseID = testService.getCourse(99000);
+		String studentList = testService.getStudentList();
+		String courseList = testService.getCourseList();
+
+		assertTrue(studentID.length() > 0);
+		assertTrue(courseID.length() > 0);
+		assertTrue(studentList.length() > 0);
+		assertTrue(courseList.length() > 0);
+
+	}
+
+	@Test
+	public void testJSONtoCSV()  throws MalformedURLException, IOException{
+
+		String studentID = testService.getStudent(111111);
+		String courseID = testService.getCourse(99000);
+		String studentList = testService.getStudentList();
+		String courseList = testService.getCourseList();
+
+		assertTrue(true);
+
+		//convert to CSV here
+
+	}
+}
