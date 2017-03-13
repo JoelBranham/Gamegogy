312ffacb0aa09c77ed97167a0f70a0978a50f93a
Steven Cortez <steven_cortez@msn.com>
2017-03-13 14:10:14 -0500

    added test for said class

---
+++ src/test/java/edu/jsu/mcis/MenuBarTest.java
@@ -0,0 +1,13 @@
+package edu.jsu.mcis;
+
+import org.junit.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import static org.junit.Assert.*;
+
+public class MenuBarTest{
+
+    
+}
